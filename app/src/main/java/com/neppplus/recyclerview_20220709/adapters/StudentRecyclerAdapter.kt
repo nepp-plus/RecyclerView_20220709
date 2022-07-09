513fb37bd8b8f86475ddef6f91a11cb0e5d6dcd7
nepp-plus <nepp.plus@gmail.com>
2022-07-09 10:38:22 +0900

    리싸이클러 어댑터 작업중  - Recycler.Adapter 상속 => 자료형에 ViewHolder 필요  - 어떤 ViewHolder인지 클래스 자체 정의 중

---
+++ app/src/main/java/com/neppplus/recyclerview_20220709/adapters/StudentRecyclerAdapter.kt
@@ -0,0 +1,12 @@
+package com.neppplus.recyclerview_20220709.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class StudentRecyclerAdapter : RecyclerView.Adapter<>() {
+
+    inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+    }
+
+}
