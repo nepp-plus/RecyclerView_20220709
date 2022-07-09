cfe12a8b000d8c1ef2e4f89c9639c3339aca8c0e
nepp-plus <nepp.plus@gmail.com>
2022-07-09 10:43:53 +0900

    어댑터의 자료형으로 뷰 홀더 설정 / 필수 구현 함수 3개 오버라이딩

--- app/src/main/java/com/neppplus/recyclerview_20220709/adapters/StudentRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220709/adapters/StudentRecyclerAdapter.kt
@@ -1,11 +1,24 @@
 package com.neppplus.recyclerview_20220709.adapters
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class StudentRecyclerAdapter : RecyclerView.Adapter<>() {
+class StudentRecyclerAdapter : RecyclerView.Adapter<StudentRecyclerAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
 
     }
 
