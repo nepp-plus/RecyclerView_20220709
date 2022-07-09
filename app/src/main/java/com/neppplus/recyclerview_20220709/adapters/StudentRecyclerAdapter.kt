1e566744616fbb9dcb2a49a42a1454e062630c5e
nepp-plus <nepp.plus@gmail.com>
2022-07-09 11:07:08 +0900

    onCreateViewHolder (xml / ViewHolder 연결) + getItemCount (몇개의 아이템) 함수 완성

--- app/src/main/java/com/neppplus/recyclerview_20220709/adapters/StudentRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220709/adapters/StudentRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.neppplus.recyclerview_20220709.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.recyclerview_20220709.R
 import com.neppplus.recyclerview_20220709.datas.StudentData
 
 class StudentRecyclerAdapter(
@@ -17,14 +19,20 @@ class StudentRecyclerAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
+//        어떤 xml을 끌어와서 => MyViewHolder에 넣어서 리턴할 것인가 : LayoutInflater 활용
+
+        val row = LayoutInflater.from(mContext).inflate( R.layout.student_list_item, parent, false )
+        return MyViewHolder(row)
+
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
     }
 
-    override fun getItemCount(): Int {
 
-    }
+//    총 몇개의 아이템인가 => mList의 size 변수 활용
+
+    override fun getItemCount() = mList.size
 
 }
