26bfae448b4e06476cad599a01bbcb224da6be2f
nepp-plus <nepp.plus@gmail.com>
2022-07-09 10:16:23 +0900

    데이터 바인딩 세팅부터 진행

--- app/src/main/java/com/neppplus/recyclerview_20220709/MainActivity.kt
+++ app/src/main/java/com/neppplus/recyclerview_20220709/MainActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.recyclerview_20220709
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.recyclerview_20220709.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
